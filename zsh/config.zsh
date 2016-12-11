91b81095fecdf4a8c5bb86f10bb2d2d2ca8b2e11
Tyson Wolker <tysonwolker@gmail.com>
2016-12-11 21:14:17 +1100

    Added new git aliases and vim alias

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -31,6 +31,9 @@ setopt HIST_REDUCE_BLANKS
 #   like: git comm-[tab]
 setopt complete_aliases
 
+zle -N newtab
+
+bindkey '^[[Z' reverse-menu-complete
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
 bindkey '^[[5D' beginning-of-line
