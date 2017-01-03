84915a4e0b479f7df21a279917c91a38568a3fe9
Tyson Wolker <tyson.wolker@foxsports.com.au>
2017-01-03 17:53:48 +1100

    Wanted to try grunt plugin for zsh, and fixed vim indent

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,3 +6,4 @@ zplug "tmuxinator/tmuxinator", use:"completion/tmuxinator.zsh", defer:3
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-autosuggestions", defer:2
 zplug "zsh-users/zsh-syntax-highlighting", defer:3 # Should be loaded 2nd last.
+zplug "yonchu/grunt-zsh-completion"
