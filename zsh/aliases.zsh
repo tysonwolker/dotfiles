ca17e19b90ac7fce17d3532152060f933cbac426
Tyson Wolker <tysonwolker@gmail.com>
2017-04-21 11:00:43 +1000

    Added spacevim, maven, mas and a few alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,9 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# Directory navigation
+alias ...='../../'
+alias ....='../../../'
+alias .....='../../../../'
+alias ......= '../../../../../'
