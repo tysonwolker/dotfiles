f62b6703d499321f66a66d4261a229ade7cd0bf3
Tyson Wolker <tysonwolker@gmail.com>
2018-02-11 21:46:07 +1100

    Added networkon/networkoff commands
    These commands are to turn on/off wifi connections.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,5 +9,5 @@ alias .....='../../../../'
 alias ......= '../../../../../'
 
 # Network bounce 
-alias networkbounce='sudo networksetup -setv4off Wi-Fi;sudo  networksetup -setdhcp Wi-Fi'
-
+alias networkoff='sudo networksetup -setv4off Wi-Fi'
+alias networkon='sudo networksetup -setdhcp Wi-Fi'
